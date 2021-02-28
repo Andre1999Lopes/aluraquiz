ef08634a1fdbc63ce88d42967fbc9b84ac5271b4
Andre1999Lopes <andrejuniorlopes@gmail.com>
2021-02-28 00:34:32 -0300

    Código ao final da primeira aula da Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import GithubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg} >
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>The Legend of Zelda</h1>
+            </Widget.Header>
+            <p>lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>Quiz da galera</h1>
+            </Widget.Header>
+            <p>lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl = '/https://www.github.com/Andre1999Lopes/aluraquiz'/>
+    </QuizBackground>
+  )
 }
